952be17a168e42674d56429324823c875ffea648
Tanishkpandey <pandeytanishk@gmail.com>
2024-04-07 08:41:02 +0530

    Router animations dosen't looks good

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,22 @@ import Error404 from "./pages/Error404";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Food from "./pages/Food";
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <TransitionGroup>
-        <CSSTransition key={window.location.key} classNames="fade" timeout={300}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/food" element={<Food />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/contactus" element={<ContactForm />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<Error404 />} />
-          </Routes>
-        </CSSTransition>
-      </TransitionGroup>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contactus" element={<ContactForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+
       <Footer />
     </div>
   );
